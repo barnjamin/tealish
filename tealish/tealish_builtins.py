eee16bf6aeab32c6fd72426ad14a4985c30fc3e5
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-01-15 08:39:34 -0500

    idk

--- tealish/tealish_builtins.py
+++ tealish/tealish_builtins.py
@@ -25,16 +25,26 @@ def get_struct(struct_name: str) -> "Struct":
     return structs[struct_name]
 
 
-constants: Dict[str, Tuple[AVMType, Union[str, bytes, int]]] = {
+on_completes: Dict[str, Tuple[AVMType, int]] = {
     "NoOp": (AVMType.int, 0),
     "OptIn": (AVMType.int, 1),
     "CloseOut": (AVMType.int, 2),
     "ClearState": (AVMType.int, 3),
     "UpdateApplication": (AVMType.int, 4),
     "DeleteApplication": (AVMType.int, 5),
+}
+
+txn_types: Dict[str, Tuple[AVMType, int]] = {
     "Pay": (AVMType.int, 1),
+    "Keyreg": (AVMType.int, 2),
     "Acfg": (AVMType.int, 3),
     "Axfer": (AVMType.int, 4),
     "Afrz": (AVMType.int, 5),
     "Appl": (AVMType.int, 6),
+    "Stpf": (AVMType.int, 7),
+}
+
+constants: Dict[str, Tuple[AVMType, Union[str, bytes, int]]] = {
+    **on_completes,
+    **txn_types,
 }
